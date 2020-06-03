88e5b2198dcb75f88a3b11a79ba5594896882e43
grep-safi <hub.of.safi@gmail.com>
2020-06-02 18:27:00 -0700

    Used tinyurl in redirect link

--- src/HydrogenAtomSimulator.jsx
+++ src/HydrogenAtomSimulator.jsx
@@ -158,11 +158,10 @@ export default class HydrogenAtomSimulator extends React.Component {
                         <p id={"energyLabel"}><i>Energy</i></p>
 
                         <div id="survey">
-                            <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=n7L3RQCxQUyAT7NBighZStjAWTIFlutChq8ZZEGLLMdUNTJKNlVITzlCNTAyNDJFQkwwTlFFNE80Vi4u" target="_blank" rel="noopener noreferrer">
+                            <a href="https://tinyurl.com/y9vf4uep" target="_blank" rel="noopener noreferrer">
                                 <button type="button" className="btn btn=warning" id="feedback">Give us feedback!</button>
                             </a>
                         </div>
-
                     </div>
 
                     <div className={"EventLog"}>
